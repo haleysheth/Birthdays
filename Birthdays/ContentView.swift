55006d289483e19fc1331511dda0774450d01dfa
Haley Sheth <haley.sheth@gmail.com>
2025-07-25 10:59:40 -0400

    added swiftdata

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -5,20 +5,19 @@
 // Created by Scholar on 7/25/25.
 //
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
     
-    @State private var friends:
-    [Friend] = [
-      Friend(name: "Taylor Swift", birthday: .now),
-      Friend(name: "Frank Ocean", birthday: Date(timeIntervalSince1970:0))
-  ]
+    @Query private var friends: [Friend]
+    @Environment(\.modelContext) private var context
+    
     @State private var newName = ""
     @State private var newBirthday = Date.now
     
   var body: some View {
     NavigationStack{
-      List(friends, id:\.name){ friend in
+      List(friends){ friend in
         HStack{
           Text(friend.name)
           Spacer()
@@ -38,7 +37,7 @@ struct ContentView: View {
               
               Button("Save") {
                   let newFriend = Friend(name: newName, birthday: newBirthday)
-                  friends.append(newFriend) // everytime we add that friend name it'll add as a new friend
+                  context.insert(newFriend)
                   
                   newName = ""
                   newBirthday = .now
@@ -56,4 +55,5 @@ struct ContentView: View {
 }
 #Preview {
   ContentView()
+        .modelContainer(for: Friend.self, inMemory: true)
 }
--- Birthdays/Friend.swift
+++ Birthdays/Friend.swift
@@ -5,7 +5,9 @@
 // Created by Scholar on 7/25/25.
 //
 import Foundation
+import SwiftData
 
+@Model
 class Friend {
   var name: String
   var birthday: Date
