7e3821394372cf602a0bc3943a8684d237445323
Haley Sheth <haley.sheth@gmail.com>
2025-07-25 10:26:18 -0400

    stage one of birthday app done.

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -1,24 +1,59 @@
 //
-//  ContentView.swift
-//  Birthdays
+// ContentView.swift
+// Birthdays
 //
-//  Created by Scholar on 7/25/25.
+// Created by Scholar on 7/25/25.
 //
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    
+    @State private var friends:
+    [Friend] = [
+      Friend(name: "Taylor Swift", birthday: .now),
+      Friend(name: "Frank Ocean", birthday: Date(timeIntervalSince1970:0))
+  ]
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
+    
+  var body: some View {
+    NavigationStack{
+      List(friends, id:\.name){ friend in
+        HStack{
+          Text(friend.name)
+          Spacer()
+          Text(friend.birthday, format: .dateTime.month(.wide).day().year())
         }
-        .padding()
+      }
+      //Text("hi")
+      .navigationTitle("Birthdays")
+      .safeAreaInset(edge : .bottom){
+          VStack(alignment: .center, spacing: 20){
+              Text("New Birthday")
+                  .font(.headline)
+              DatePicker(selection: $newBirthday, in: Date.distantPast...Date.now, displayedComponents: .date){
+                  TextField("Name", text:$newName)
+                      .textFieldStyle(.roundedBorder)
+              }
+              
+              Button("Save") {
+                  let newFriend = Friend(name: newName, birthday: newBirthday)
+                  friends.append(newFriend) // everytime we add that friend name it'll add as a new friend
+                  
+                  newName = ""
+                  newBirthday = .now
+              }
+              .bold()
+              
+            
+          }
+          .padding()
+          .background(.bar)
+          
+      }
     }
+  }
 }
-
 #Preview {
-    ContentView()
+  ContentView()
 }
